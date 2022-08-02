8e7c1fe3f2fd80ddda9a017d07487f798628c444
Nooha <noohakottangodan@gmail.com>
2022-08-02 22:35:26 +0530

    shm

---
+++ Shared Memory/shmread.c
@@ -0,0 +1,43 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include<unistd.h>
+
+typedef struct StudentDetails
+{
+    char name[10];
+    int rank;
+    int count;
+} Student;
+
+void main()
+{
+    key_t key = ftok("shmfile", 65);
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+    Student st[10];
+    int n;
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nDetails of Student %d\n", i);
+        printf("Enter name: ");
+        scanf("%s", st[i].name);
+        printf("Enter Mark: ");
+        scanf("%d", &st[i].rank);
+        st[i].count = n;
+    }
+
+    Student *data = (Student *)shmat(shmid, NULL, 0);
+
+    memcpy(data, st, 10 * sizeof(Student));
+
+    shmdt(data);
+    printf("\nSuccessfully copied to the memory\n");
+
+    char *args[]={"./shmdisplay",NULL};
+    execvp(args[0],args);
+}
